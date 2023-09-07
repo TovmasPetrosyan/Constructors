78f8d4db8e4daeeec4d11784875c115ab07d4c67
Tovmas Petrosyan <tompetrosyan30@gmail.com>
2023-09-07 20:27:04 +0400

    Create functions for the Calculator class that can do the following: Add two numbers. Subtract two numbers. Multiply two numbers. Divide two numbers

---
+++ CalculatorConstructor.js
@@ -0,0 +1,33 @@
+//Create functions for the Calculator class that can do the following:
+//Add two numbers.
+//Subtract two numbers.
+//Multiply two numbers.
+//Divide two numbers.
+
+
+
+function Calculator(){    
+   
+}
+
+
+Calculator.prototype.add = function(a,b){
+    
+    return a + b;
+}
+
+Calculator.prototype.subtract = function(a,b){
+    return a - b;
+}
+Calculator.prototype.multiply = function(a,b){
+    return a * b;
+}
+Calculator.prototype.divide = function(a,b){
+    return a / b;
+}
+//const calculator = new Calculator();
+
+//console.log(calculator.add(10, 5));
+//console.log(calculator.subtract(10, 5));
+//console.log(calculator.multiply(10, 5));
+//console.log(calculator.divide(10, 5));
